ec506371148a79d08c868cecc69d1e53d15c05e8
Bernard Mark <Bernard Mark>
2017-01-17 16:45:51 -0800

    Added updated AppBasics.java file

--- src/main/java/com/sqa/bm/helpers/AppBasics.java
+++ src/main/java/com/sqa/bm/helpers/AppBasics.java
@@ -7,6 +7,8 @@
  */
 package com.sqa.bm.helpers;
 
+import java.util.*;
+
 /**
  * AppBasics //ADDD (description of class)
  * <p>
@@ -19,21 +21,29 @@ package com.sqa.bm.helpers;
  * @since 1.0
  */
 public class AppBasics {
-	}
 
 	static Scanner scanner = new Scanner(System.in);
-
-	public static String greetUserAndGetName(String nameOfApp) {
-		System.out.println("Hello, Welcome to the " + nameOfApp + " App.");
-		// because it's a string, program knows nameOfApp is same as appName//
-		System.out.print("Could I please get your name? ");
-		// println add carriage return or \n"//
-		return scanner.nextLine();
-		// nextLine is a method //
 
 	public static void farwellUser(String userName, String appName) {
 		// TODO Auto-generated method stub
 		System.out.println("Thank you for using the " + appName + " application");
 		System.out.println("Have a good day " + userName);
+	}
+
+	public static String greetUserAndGetName(String appName) {
+		System.out.println("Hello, Welcome to the " + appName + " App.");
+		// because it's a string, program knows nameOfApp is same as appName//
+		System.out.print("Could I please get your name? ");
+		// println add carriage return or \n"//
+		return scanner.nextLine();
+		// nextLine is a method //
+	}
+
+	@SuppressWarnings("javadoc")
+	public static String requestInfoFromUser(String info) {
+		String response;
+		System.out.println(info + " ");
+		response = scanner.nextLine();
+		return response;
 	}
 }
